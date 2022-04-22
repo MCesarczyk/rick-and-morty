11d4b9ecebfb2032f5457c72aba98bbb00f8e96e
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-04-22 18:38:59 +0200

    wrapped form with formik

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -5,24 +5,57 @@ import {
   FormLabel,
   Input
 } from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+
+type innerFormProps = {
+  field: object,
+  form: {
+    errors: {
+      name: boolean
+    },
+    touched: {
+      name: boolean
+    }
+  }
+}
 
 const SearchForm = () => (
-  <FormControl
-    m={4}
-    display="flex"
-    alignItems="baseline"
+  <Formik
+    initialValues={{ name: "" }}
+    onSubmit={(values, actions) => {
+      setTimeout(() => {
+        console.log(values.name);
+        actions.setSubmitting(false);
+      }, 200);
+    }}
   >
-    <FormLabel htmlFor="name" mx={4}>Search&nbsp;by&nbsp;name:</FormLabel>
-    <Input id="name" placeholder="name" />
-    <Button
-      mx={4}
-      px={8}
-      colorScheme="teal"
-      type="submit"
-    >
-      Submit
-    </Button>
-  </FormControl>
+    {(props) => (
+      <Form>
+        <Field name="name">
+          {({ field, form }: innerFormProps) => (
+            <FormControl
+              m={4}
+              display="flex"
+              alignItems="baseline"
+              isInvalid={form.errors.name && form.touched.name}
+            >
+              <FormLabel htmlFor="name" mx={4}>Search&nbsp;by&nbsp;name:</FormLabel>
+              <Input {...field} id="name" placeholder="name" />
+              <Button
+                mx={4}
+                px={8}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </FormControl>
+          )}
+        </Field>
+      </Form>
+    )}
+  </Formik>
 );
 
 export default SearchForm;
