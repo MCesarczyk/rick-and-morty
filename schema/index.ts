7ec1ed56f69942a0ae383dddeb38093c3c1a4fa1
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-12-08 00:14:11 +0100

    updated path for generated content

--- schema/index.ts
+++ schema/index.ts
@@ -1,8 +1,13 @@
 import { makeSchema } from 'nexus';
 import * as QueryTypes from './Query';
+import path from 'path';
 
 const schema = makeSchema({
   types: [QueryTypes],
+  outputs: {
+    schema: path.join(__dirname, '/generated/schema.graphql'),
+    typegen: path.join(__dirname, '/generated/types.ts'),
+  }
 });
 
 export default schema;
